62315989cbb6879584b65b917219dc9f202a1148
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-08-03 16:45:29 +0100

    [project] Automatically build samples using build.bat

---
+++ Samples/build.bat
@@ -0,0 +1,8 @@
+﻿
+set dsc="..\DogeSharp\bin\Debug\dsc"
+
+echo Building console sample
+%dsc% Main.ds MyType.ds /out:ConsoleApp.exe /preservetranslated
+
+echo Building WinForms sample
+%dsc% WinForms.ds /target:winexe /out:doge.exe /reference:System.Windows.Forms.dll /reference:System.dll /preservetranslated
