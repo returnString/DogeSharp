e24f6e4ea02b78bedb09153de0e988ed35f87253
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-08-03 16:48:26 +0100

    [project] Corrected name of WinForms binary

--- Samples/build.bat
+++ Samples/build.bat
@@ -5,4 +5,4 @@ echo Building console sample
 %dsc% Main.ds MyType.ds /out:ConsoleApp.exe /preservetranslated
 
 echo Building WinForms sample
-%dsc% WinForms.ds /target:winexe /out:doge.exe /reference:System.Windows.Forms.dll /reference:System.dll /preservetranslated
+%dsc% WinForms.ds /target:winexe /out:FormsApp.exe /reference:System.Windows.Forms.dll /reference:System.dll /preservetranslated
