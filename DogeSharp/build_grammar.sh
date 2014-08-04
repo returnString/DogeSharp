0c039354c211ebfaeb0714c0f3dc403aa0852d3a
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-08-05 00:12:46 +0100

    [project] Added a shell script to regenerate the grammar for Unix

---
+++ DogeSharp/build_grammar.sh
@@ -0,0 +1,1 @@
+java -cp ../packages/Antlr4.4.3.0/tools/antlr4-csharp-4.3-complete.jar org.antlr.v4.CSharpTool -o obj/Debug -listener -visitor -Dlanguage=CSharp_v4_5 -package DogeSharp DogeSharp.g4 
