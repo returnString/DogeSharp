403730a9cc4eb7674188f1b4b68aea55b09a3ee6
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-08-04 20:29:18 +0100

    [samples] Added Servya D# sample for wow-as-a-service

--- Samples/build.bat
+++ Samples/build.bat
@@ -1,7 +1,12 @@
 set dsc="..\DogeSharp\bin\Debug\dsc"
 
+xcopy /y Libs\*.dll
+
 echo Building console sample
 %dsc% Main.ds MyType.ds /out:ConsoleApp.exe /preservetranslated
 
 echo Building WinForms sample
 %dsc% WinForms.ds /target:winexe /out:FormsApp.exe /reference:System.Windows.Forms.dll /reference:System.dll /preservetranslated
+
+echo Building Servya sample
+%dsc% Server.ds /out:ServyaApp.exe /reference:Libs\Servya.dll /reference:Libs\Servya.Http.dll /reference:System.dll /preservetranslated
