9c7ab0f929b48f03a903a05d9f3ea93cabd92cf8
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-08-05 00:08:24 +0100

    [project] Link generated files into dsc project, fixes Xamarin Studio builds, added shell build script

---
+++ Samples/build.sh
@@ -0,0 +1,13 @@
+dsc="mono ../DogeSharp/bin/Debug/dsc.exe"
+
+cp Libs/*.dll .
+
+echo Building console sample
+$dsc Main.ds MyType.ds /out:ConsoleApp.exe /preservetranslated
+
+echo Building WinForms sample
+$dsc WinForms.ds /target:winexe /out:FormsApp.exe /reference:System.Windows.Forms.dll /reference:System.dll /preservetranslated
+
+echo Building Servya sample
+$dsc Server.ds /out:ServyaApp.exe /reference:Libs/Servya.dll /reference:Libs/Servya.Http.dll /reference:System.dll /preservetranslated
+
